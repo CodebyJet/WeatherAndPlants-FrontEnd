170a1db1ab3fff8f234f3638982565d684f199f2
Jet <thazledine@hotmail.co.uk>
2023-04-24 22:53:43 +0100

    refectored what we had

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
-import React, { useEffect } from 'react';
-import { API } from './components/lib/api';
-import { useState } from 'react';
+import DisplayWeather from "./components/displayWeather";
 
 function App() {
 
-  const [displayLocation, setDisplayLocation] = useState(null)
-  const [isUpdated, setIsUpdated] = useState(false);
+const city = 'london';
 
-
-  const area = 'london'
-
-
-useEffect(()=> {
-  API.GET(API.ENDPOINTS.singleWeather(area), API.Access).then(({data}) => {
-    setDisplayLocation(data)
-  }).catch((error)=>{
-    console.log(error)
-  })
-    setIsUpdated(false);
-  }, [isUpdated]);
-
-  console.log(displayLocation);
-
-  return (<><div>App</div><div>{displayLocation?.current.condition.text}</div></>);
+if (city){
+  return (
+    <>
+    <DisplayWeather city={city}  />
+    </>
+  );
+}else{
+  return <div>Loading...</div>
+}
 }
 
 export default App;
---
+++ src/components/displayWeather.js
@@ -0,0 +1,32 @@
+import { useEffect } from 'react';
+import { API } from './lib/api';
+import { useState } from 'react';
+
+export default function DisplayWeather(city) {
+  const [displayLocation, setDisplayLocation] = useState(null);
+  const [isUpdated, setIsUpdated] = useState(false);
+
+  useEffect(() => {
+    API.GET(API.ENDPOINTS.singleWeather(city.city), API.Access)
+      .then(({ data }) => {
+        setDisplayLocation(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    setIsUpdated(false);
+  }, [isUpdated]);
+
+  return (
+    <>
+      <div>{displayLocation?.location.name}</div>
+      <img
+        src={displayLocation?.current.condition.icon}
+        alt={displayLocation?.current.condition.text}
+      />
+      <div>{displayLocation?.current.condition.text}</div>
+      <div>Temp: {displayLocation?.current.temp_c}</div>
+      <div>Feels like: {displayLocation?.current.feelslike_c}</div>
+    </>
+  );
+}
