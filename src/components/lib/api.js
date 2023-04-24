6006d67fda72a1c6b9202b8c84af36eeeacab5ff
Jet <thazledine@hotmail.co.uk>
2023-04-24 16:04:25 +0100

    mock single weather

--- src/components/lib/api.js
+++ src/components/lib/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const BASE_URL = 'http://api.weatherapi.com/v1'; //insert the api link here
 
-export const getAllWeather = () =>
-  axios.get(`${BASE_URL}`);
+// http://api.weatherapi.com/v1/current.json?q=London
+export const getSingleWeather = (id) => axios.get(`${BASE_URL}/current.json?q=${id}`);
 
-export const getSingleWeather = (id) => axios.get(`${BASE_URL}/${id}`);
+
