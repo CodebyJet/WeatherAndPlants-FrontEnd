c09763d5ca506e9a932bf70281c3f16a87ca07c0
Jet <thazledine@hotmail.co.uk>
2023-04-24 15:43:23 +0100

    added url

--- src/components/lib/api.js
+++ src/components/lib/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const BASE_URL = ''; //insert the api link here
+export const BASE_URL = 'http://api.weatherapi.com/v1'; //insert the api link here
 
 export const getAllWeather = () =>
   axios.get(`${BASE_URL}`);
