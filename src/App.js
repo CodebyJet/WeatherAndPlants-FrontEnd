82d878d7eeced4e34320791b8e87df30e354b359
Jet <thazledine@hotmail.co.uk>
2023-04-24 17:56:37 +0100

    displays london

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { API } from './components/lib/api';
 import { useState } from 'react';
 
-
-
 function App() {
 
-  const [displayLocation, setDisplayLocation] = useState({})
+  const [displayLocation, setDisplayLocation] = useState(null)
+  const [isUpdated, setIsUpdated] = useState(false);
 
 
-  const London = 'london'
-  const Display = API.GET(API.ENDPOINTS.singleWeather, London)
-  console.log(Display)
+  const area = 'london'
 
-  return (<div>App</div>);
+
+useEffect(()=> {
+  API.GET(API.ENDPOINTS.singleWeather(area), API.Access).then(({data}) => {
+    setDisplayLocation(data)
+  }).catch((error)=>{
+    console.log(error)
+  })
+    setIsUpdated(false);
+  }, [isUpdated]);
+
+  console.log(displayLocation);
+
+  return (<><div>App</div><div>{displayLocation?.current.condition.text}</div></>);
 }
 
 export default App;
--- src/components/lib/api.js
+++ src/components/lib/api.js
@@ -1,31 +1,20 @@
 import axios from 'axios';
-import { AUTH } from './auth';
 
 export const BASE_URL = 'http://api.weatherapi.com/v1'; //insert the api link here
 
 // http://api.weatherapi.com/v1/current.json?q=London
-export const getSingleWeather = (id) => axios.get(`${BASE_URL}/current.json?q=${id}`);
-
 
 const ENDPOINTS = {
-  singleWeather: (id) => `${BASE_URL}/current.json?q=${id}`
+  singleWeather: (id) =>
+    `${BASE_URL}/current.json?key=${process.env.REACT_APP_VALUE}&q=${id}`
 };
 
 
-const getHeaders = () => ({
-  headers: {
-    Authorization: `Bearer ${AUTH.getToken()}`
-  }
-});
+const GET = (endpoint) =>
+  axios.get(
+    endpoint
+  );
 
-const Access = {
-  username: process.env.REACT_APP_USERNAME,
-  password: process.env.REACT_APP_PASSWORD,
-  key: process.env.REACT_APP_KEY,
-  value: process.env.REACT_APP_VALUE
-};
 
-const GET = (endpoint, headers, Access) =>
-  headers ? axios.get(endpoint, headers, Access) : axios.get(endpoint);
-
-  export const API = { GET, ENDPOINTS, getHeaders, Access };
+  
+  export const API = { GET, ENDPOINTS };
