11c461473cfe1c6e4f0731877b6967a411bf17e9
Jet Haze <thazledine@hotmail.co.uk>
2023-12-02 22:18:31 +0000

    trying to adjust to see where the problem is

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,25 +1,25 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { TextField, Button, Container, Select, MenuItem } from '@mui/material'; // Added Select and MenuItem
-import { API } from '../lib/api';
-import { AUTH } from '../lib/auth';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { TextField, Button, Container, Select, MenuItem } from "@mui/material"; // Added Select and MenuItem
+import { API } from "../lib/api";
+import { AUTH } from "../lib/auth";
 
 const presetProfileImages = [
-  'image1.jpg',
-  'image2.jpg',
-  'image3.jpg'
+  "image1.jpg",
+  "image2.jpg",
+  "image3.jpg",
   // Add more preset image file names as needed
 ];
 
 export default function Register() {
   const navigate = useNavigate();
   const [formFields, setFormFields] = useState({
-    email: '',
-    username: '',
-    password: '',
-    passwordConfirmation: '',
-    location: '',
-    imageId: ''
+    email: "",
+    username: "",
+    password: "",
+    passwordConfirmation: "",
+    location: "",
+    imageId: "",
   });
   const [error, setError] = useState(false);
 
@@ -40,12 +40,12 @@ export default function Register() {
 
       const loginData = await API.POST(API.ENDPOINTS.login, {
         email: formFields.email,
-        password: formFields.password
+        password: formFields.password,
       });
 
       AUTH.setToken(loginData.data.token);
 
-      navigate('/');
+      navigate("/");
     } catch (e) {
       console.log(e);
       setError(true);
@@ -54,17 +54,17 @@ export default function Register() {
 
   return (
     <Container
-      maxWidth='lg'
-      sx={{ display: 'flex', justifyContent: 'center', pt: '200px' }}
+      maxWidth="lg"
+      sx={{ display: "flex", justifyContent: "center", pt: "200px" }}
     >
       <form onSubmit={handleCreateUser}>
         <div>
           <TextField
-            size='small'
-            name='username'
-            id='username'
-            type='text'
-            label='Username'
+            size="small"
+            name="username"
+            id="username"
+            type="text"
+            label="Username"
             required={true}
             value={formFields.username}
             onChange={handleChange}
@@ -74,11 +74,11 @@ export default function Register() {
         </div>
         <div>
           <TextField
-            size='small'
-            name='email'
-            id='email'
-            type='email'
-            label='Email'
+            size="small"
+            name="email"
+            id="email"
+            type="email"
+            label="Email"
             required={true}
             value={formFields.email}
             onChange={handleChange}
@@ -88,11 +88,11 @@ export default function Register() {
         </div>
         <div>
           <TextField
-            size='small'
-            name='password'
-            id='password'
-            type='password'
-            label='Password'
+            size="small"
+            name="password"
+            id="password"
+            type="password"
+            label="Password"
             required={true}
             value={formFields.password}
             onChange={handleChange}
@@ -102,11 +102,11 @@ export default function Register() {
         </div>
         <div>
           <TextField
-            size='small'
-            name='passwordConfirmation'
-            id='passwordConfirmation'
-            type='password'
-            label='Password Confirmation'
+            size="small"
+            name="passwordConfirmation"
+            id="passwordConfirmation"
+            type="password"
+            label="Password Confirmation"
             required={true}
             value={formFields.passwordConfirmation}
             onChange={handleChange}
@@ -115,18 +115,18 @@ export default function Register() {
           />
         </div>
         <div>
-          <Select  //the images selected from preset
-            size='small'
-            name='imageId'
-            id='imageId'
-            label='Profile Image'
+          <Select //the images selected from preset
+            size="small"
+            name="imageId"
+            id="imageId"
+            label="Profile Image"
             required={true}
             value={formFields.imageId}
             onChange={handleProfileImageChange}
             error={error}
             sx={{ mb: 2 }}
           >
-            <MenuItem value=''>Select an Image</MenuItem>
+            <MenuItem value="">Select an Image</MenuItem>
             {presetProfileImages.map((imageName, index) => (
               <MenuItem key={index} value={imageName}>
                 {imageName}
@@ -135,12 +135,12 @@ export default function Register() {
           </Select>
         </div>
         <div>
-          <TextField //though maybe needs to be a selector if they spell it wrong?
-            size='small'
-            name='location'
-            id='location'
-            type='location'
-            label='location'
+          <TextField // though maybe needs to be a selector if they spell it wrong?
+            size="small"
+            name="location"
+            id="location"
+            type="location"
+            label="location"
             required={true}
             value={formFields.location}
             onChange={handleChange}
@@ -148,8 +148,8 @@ export default function Register() {
             sx={{ mb: 2 }}
           />
         </div>
-        <Button type='submit' sx={{ color: '#3B3D40' }}>
-          Create Account
+        <Button type="submit" sx={{ color: "#3B3D40" }}>
+          Create Account!
         </Button>
       </form>
     </Container>
