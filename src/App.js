64c91ba21d91789e221ece71e4ba42e3254530cb
Jet Haze <thazledine@hotmail.co.uk>
2023-12-10 22:34:38 +0000

    fleshed out plant cards and tidies up the main page

--- src/App.js
+++ src/App.js
@@ -1,34 +1,19 @@
-import DisplayWeather from './components/displayWeather';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import DisplayPlantList from './components/displayAllPlants';
-import DisplaySinglePlant from './components/displaySinglePlant';
 import Register from './components/Register';
 
 window.Buffer = window.Buffer || require('buffer').Buffer;
 
 function App() {
-  const city = 'Manchester';
-  const searchedPlant = 1
-  if (city) {
     return (
-      <div>
       <Router>
         <Routes>
           <Route path='/register' element={<Register />} />
           <Route path='/plants' element={<DisplayPlantList />} />
         </Routes>
       </Router>
-      <div>
-        <DisplayWeather city={city}/>
-        <DisplayPlantList />
-        <DisplaySinglePlant plantId={searchedPlant}/>
-      </div>
-      </div>
     );
-  } else {
-    return <div>Loading...</div>;
-  }
 }
 
 export default App;
